5c188b4dc3d82611ba9b423d23f33b79271c2183
Claudio Catalani <50852422+CatalaniCD@users.noreply.github.com>
2021-06-27 08:39:52 -0400

    Add files via upload

--- neuron.py
+++ neuron.py
@@ -27,6 +27,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+import time
+
 
 # =============================================================================
 # Neuron Class
@@ -39,12 +41,16 @@ class Neuron():
     def __init__(self):
         self.weights = np.array([])
         self.bias = 0
-        self.learning_rate = 1e-03
-        self.epsilon = 1e-03
+        self.learning_rate = 0.1
+        self.epsilon = 0.1
         
-    def scalar_product(self, X):
-        """ Dot Product / Matmul """
-        return np.dot(self.weights.T, X) + self.bias
+    
+    def loss_function(self, y, y_hat):
+        """ Loss Function : Squared Loss """
+        return 0.5 * (y - y_hat) ** 2
+    
+    def loss_derivative(self, y, y_hat):
+        return - (y - y_hat)
     
     def sigmoid(self, z):
         """ Activation Function """
@@ -52,22 +58,28 @@ class Neuron():
         return 1 / (1 + np.exp(-z))
     
     def sigmoid_derivative(self, z):
-      """ Activation Derivative """
-      return self.sigmoid(z) * (1 - self.sigmoid(z))
-  
-    def loss_function(self, y, y_hat):
-        """ Loss Function : Squared Loss """
-        return 0.5 * (y - y_hat) ** 2
-    
-    def loss_derivative(self, y, y_hat):
-        """ Loss derivative """
-        return - (y - y_hat)
+        """ Activation Derivative """
+        return self.sigmoid(z) * (1 - self.sigmoid(z))
+
+    def forward_prop(self, X_):
+        self.z = self.weights @ X_ + self.bias
+        return self.sigmoid(self.z)
+   
+    def back_prop(self, X_, y_, activation):
+        # derivatives
+        loss_derivative = self.loss_derivative(y_, activation)
+        activation_derivative = self.sigmoid_derivative(self.z)
+        # updates                                        
+        weights_update =  loss_derivative * activation_derivative * X_
+        bias_update = ( loss_derivative * activation_derivative * 1 ).sum( axis = 0 , keepdims = True)
+        return weights_update, bias_update
+        
           
-    def training(self, X, y, T = 25):
+    def training(self, X, y, T = 100, VISUALIZE = False):
         """ Model Training """
         # initialize random weights and bias
-        self.weights = np.random.normal(loc = 0.0, scale = 5.0, size = X.shape[1])
-        self.bias = np.random.randint(1)      
+        self.weights = np.random.uniform(low = -1, high = 1, size = X.shape[1]) 
+        self.bias = np.random.uniform(low = -1, high = 1, size = None)   
         L = X.shape[0]
         update = 0
         for t in range(T):
@@ -78,18 +90,17 @@ class Neuron():
                 y_ = y[i]
                 
                 # neuron activation            
-                z = self.scalar_product(X_)
-                activation = self.sigmoid(z)
+                activation = self.forward_prop(X_)
                 
                 # loss function
                 loss = self.loss_function(y_, activation)
-    
-                # print(loss)
+               
     
                 # check for loss magnitude
                 if loss > self.epsilon:
                     
                     """
+                    
                     # derivatives
                     loss_derivative
                     activation_derivative
@@ -101,22 +112,43 @@ class Neuron():
                     # new wights, bias
                     weigths = weights - ( learning_rate * weights_update )
                     bias = bias - (learning_rate * bias_update.sum( axis = 0 ) )
+                    
+                
                     """
-                    
-                    # derivatives
-                    loss_derivative = self.loss_derivative(y_, activation)
-                    activation_derivative = self.sigmoid_derivative(z)
-                    
-                    # updates                                        
-                    weights_update = loss_derivative * activation_derivative * activation
-                    bias_update = ( loss_derivative * activation_derivative * 1 ).sum( axis = 0 )
+    
+                    weights_update, bias_update = self.back_prop(X_, y_, activation)
                     
                     # new wights / bias
-                    self.weigths = self.weights - ( self.learning_rate * weights_update )
+                    self.weights = self.weights - ( self.learning_rate * weights_update )
                     self.bias = self.bias - (self.learning_rate * bias_update )
                     
                     update += 1
                     
+                    W = self.weights
+                    b = self.bias
+                    
+                    # print(weights_update, W, b, t)
+                    if VISUALIZE:
+                    
+                         # calc and plot decision boundary
+                        min_x1, max_x1 = X_test[:, 0].min(), X_test[:, 0].max()
+                        
+                        n = 10
+                        x1 = [x for x in np.linspace(start = min_x1, stop = max_x1, num = n)] 
+                        
+                        # calc decision boundary  
+                        slope = -W[0] / W[1]
+                        intercept = -b / W[1]
+                        decision_boundary = [slope * x + intercept for x in x1]
+                        
+                        # visualize decision boundary 
+                        plt.plot(x1, decision_boundary, c = 'black', linestyle = 'dashed')
+                        plt.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=plt.cm.coolwarm)
+                        plt.show()
+                        
+                        time.sleep(0.01)
+                        
+                                    
         print(f'""" Training Complete with {update} updates """')
         pass           
                         
@@ -124,9 +156,8 @@ class Neuron():
     def predict(self, X_):
         """ Model Prediction """            
         # neuron activation                
-        activation = self.sigmoid(self.scalar_product(X_))
-        # activation < epsilon for classification
-        return (activation < self.epsilon).astype(int)
+        activation = self.forward_prop(X_)
+        return (activation > 0.5).astype(int)
     
     def accuracy(self, X, y):
         """ Model Accuracy """
@@ -139,12 +170,17 @@ class Neuron():
             
             # neuron activation / prediction                
             pred = self.predict(X_)
+            
             # error, y_ != predicted
             error += (y_ != pred).astype(int)
             
         accuracy = (1 - (error / L))
-        print(f'"""Model Baseline : { len([x for x in (y == 1) if x == True]) / len(y) }"""')
-        print(f'"""Model Accuracy : {accuracy}"""')
+        baseline = len([x for x in (y == 1) if x == True]) / len(y)
+        
+        print(f'"""Model Baseline : { baseline }"""')
+        print(f'"""Model Accuracy : { accuracy }"""')
+        score = (accuracy - baseline) / baseline
+        print(f'"""Model Score    : { str(score)[:5] }"""')
         pass
 
 # =============================================================================
@@ -156,7 +192,21 @@ if __name__ == '__main__':
     X, y = make_blobs(n_samples = 500, 
                       centers = 2, 
                       random_state = 0, 
-                      cluster_std = 1.0)
+                      cluster_std = 0.5)
+    
+    # y = pd.Series(y).replace(0, -1).values
+    
+    
+    # standard scaling
+    # X = pd.DataFrame(X)
+    # X = (X - X.mean()) / (X.std())
+    # X = X.values
+    
+    # min max scaling
+    X = pd.DataFrame(X)
+    rg = (X.max() - X.min())
+    X = (X - X.min()) / rg
+    X = X.values
     
     
     # train test split
@@ -170,8 +220,27 @@ if __name__ == '__main__':
     plt.show()
 
     neuron = Neuron()
-    neuron.training(X_train, y_train)
+    
+    neuron.training(X_train, y_train, VISUALIZE = False)
+    
     neuron.accuracy(X_test, y_test)
     
-    plt.scatter(X_test[:, 0], X_test[:, 1], c=[neuron.predict(x) for x in X_test], cmap=plt.cm.coolwarm)
+    W = neuron.weights
+    b = neuron.bias
+    
+     # calc and plot decision boundary
+    min_x1, max_x1 = X_test[:, 0].min(), X_test[:, 0].max()
+    
+    n = 100 
+    x1 = [x for x in np.linspace(start = min_x1, stop = max_x1, num = n)] 
+    
+    # calc decision boundary  
+    slope = -W[0] / W[1]
+    intercept = -b / W[1]
+    decision_boundary = [slope * x + intercept for x in x1]
+    
+    # visualize decision boundary 
+    plt.plot(x1, decision_boundary, c = 'black', linestyle = 'dashed')
+    plt.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=plt.cm.coolwarm)
     plt.show()
+    
